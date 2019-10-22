06fe1ef21b49cbb25c0b3483217ca78da7eac271
Martin Alley <martin@mjfaconsulting.com>
2019-10-22 15:05:08 +0100

    Mods for remote php debugging

--- src/phptest/test.php
+++ src/phptest/test.php
@@ -1,1 +1,19 @@
-<?php var_dump( array('CodeIgniter', 'php', 'phpMyAdmin', 'www.lucidar.me') ); ?>
+<html>
+<title>test1</title>
+<body>
+<?php
+
+echo 'testing 123';
+
+$name='John';
+$fruits=array('apple','pear','lemon');
+$currentFruit='';
+foreach($fruits as $fruit){
+    echo $name . ' likes ' . $fruit . '\r\n';
+}
+
+var_dump( array('CodeIgniter', 'php', 'phpMyAdmin', 'www.lucidar.me') );
+?>
+</body>
+</html>
+
